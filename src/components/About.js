bcb4ed518464e209511af054533d9417184ef10b
isaiahsikobe <isaiah.sikobe@student.moringaschool.com>
2023-02-01 16:14:14 +0100

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { image } from "../data/data";
 
-function About() {
-  return <div>About</div>;
-}
+const About = () => (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>I am a web developer who is passionate about building beautiful and functional websites. I have experience working with React, Angular, and Vue.js and I am constantly looking for new ways to improve my skills and stay up-to-date with the latest technologies.</p>
+    <img src={image} alt="I made this" />
+  </div>
+);
 
 export default About;
+
